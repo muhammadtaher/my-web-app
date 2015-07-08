d7cc1834ade3a4d8b82cd2cc554b574e298f65a9
Mohamed Taher <muhammad.taher@badrit.com>
2015-07-08 22:39:35 +0200

    add all

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_confirmation_of :password
+  validates :first_name, presence: true
+  validates_length_of :password, :minimum => 6
+end
